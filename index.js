7466b89e0fff45c64b397097e65d80e056c57a5a
Robert Bene <robcbene@hotmail.com>
2016-10-05 15:34:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-var cart;
+var cart = [];
+
+function getCart() {
+  return cart;
+}
+
+function addToCart(item) {
+  var b = Math.random() * 100
+  var c = Math.floor(b)
+  cart.push({[item]: c})
+  console.log (`${item} has been added to your cart.`);
+  return cart;
+}
+
+function viewCart() {
+var cartArray = []
+var cartPrice = Object.keys(cart)
+  if (cart.length === 0) {
+    console.log ("Your shopping cart is empty.");
+  }
+  else {
+    console.log("In your cart, you have")
+    for (let i = 0; i < cart.length; i++) {
+      var z = cart[i]
+      var h = cart[cartPrice[i]]
+      cartArray.push("z at h")
+      return cartArray;
+    }  } }
 
 function setCart(newCart) {
   cart = newCart;
